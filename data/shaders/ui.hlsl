426a7e389c83f47883ebbfc42f54edaae97a25b5
Carlos Rivera <cr4185320@gmail.com>
2024-07-13 00:32:10 -0500

    Update ui.hlsl

--- data/shaders/ui.hlsl
+++ data/shaders/ui.hlsl
@@ -3,14 +3,14 @@ cbuffer Constants : register(b0) {
 };
 
 struct Vertex_In {
-    float2 p_l    : POSITION;
-    float2 uv     : TEXCOORD;
+    float2 dst    : POSITION;
+    float2 src    : TEXCOORD;
     float4 color  : COLOR;
 };
 
 struct Vertex_Out {
-    float4 p_h    : SV_POSITION;
-    float2 uv     : TEXCOORD;
+    float4 pos_h  : SV_POSITION;
+    float2 tex    : TEXCOORD;
     float4 color  : COLOR;
 };
 
@@ -19,14 +19,14 @@ SamplerState tex_sampler : register(s0);
 
 Vertex_Out vs_main(Vertex_In input) {
     Vertex_Out output;
-    output.p_h = mul(xform, float4(input.p_l, 0.0, 1.0));
-    output.uv = input.uv;
+    output.pos_h = mul(xform, float4(input.dst, 0.0, 1.0));
+    output.tex = input.src;
     output.color = input.color;
     return output;
 }
 
 float4 ps_main(Vertex_Out input) : SV_TARGET {
-    float4 tex_color = tex.Sample(tex_sampler, input.uv);
+    float4 tex_color = tex.Sample(tex_sampler, input.tex);
     float4 color = input.color * tex_color;
     return color;
 }
