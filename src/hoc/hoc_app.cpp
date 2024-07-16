f2880ac503c01e1a37331c67f061cfd04dc4f54b
Carlos Rivera <cr4185320@gmail.com>
2024-07-15 20:36:43 -0500

    Update hoc_app.cpp

--- src/hoc/hoc_app.cpp
+++ src/hoc/hoc_app.cpp
@@ -1,1 +1,16 @@
 global Hoc_Application *hoc_app;
+global bool window_should_close;
+
+internal Hoc_Application *make_hoc_application() {
+    Arena *arena = make_arena(get_malloc_allocator());
+    Hoc_Application *app = push_array(arena, Hoc_Application, 1);
+    app->arena = arena;
+    app->buffer_id_counter = 0;
+    app->view_id_counter = 0;
+    return app;
+}
+
+internal void quit_hoc_application() {
+    window_should_close = true;
+}
+
