4e0409f43628bb7484861555be3c75d6a4c21d07
Carlos Rivera <cr4185320@gmail.com>
2024-07-26 15:24:22 -0500

    add shell32.lib

--- build.bat
+++ build.bat
@@ -9,8 +9,8 @@ if not exist bin mkdir bin
 set WARNING_FLAGS=/W4 /wd4100 /wd4101 /wd4189 /wd4201 /wd4456 /wd4505 /wd4706
 set INCLUDES=/Isrc\ /Iext\ /Iext\freetype\include\
 set COMMON_COMPILER_FLAGS=/nologo /FC /EHsc /Fdbin\ /Fobin\ %WARNING_FLAGS% %INCLUDES%
-set COMPILER_FLAGS=%COMMON_COMPILER_FLAGS%
 
+set COMPILER_FLAGS=%COMMON_COMPILER_FLAGS%
 if %MODE%==release (
   set COMPILER_FLAGS=/O2 /D %COMPILER_FLAGS%
 ) else if %mode%==debug (
@@ -19,7 +19,9 @@ if %MODE%==release (
   echo Unkown build mode
   exit /B 2
 )
-set LIBS=/LIBPATH:ext\freetype\libs\x64\Release\ user32.lib kernel32.lib winmm.lib shlwapi.lib freetype.lib
+
+rem set COMPILER_FLAGS=%COMPILER_FLAGS% /DHOC_ENTRY_WIN32
+set LIBS=/LIBPATH:ext\freetype\libs\x64\Release\ user32.lib shell32.lib kernel32.lib winmm.lib shlwapi.lib freetype.lib
 set COMMON_LINKER_FLAGS=/INCREMENTAL:NO /OPT:REF
 set LINKER_FLAGS=%COMMON_LINKER_FLAGS% %LIBS%
 
